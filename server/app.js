d5db597ab92de00d6b58fbccb812f346d3a49591
michael byalsky <michael.byalsky@gmail.com>
2020-11-25 23:42:17 +0200

    challenges-doc

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 app.use(express.json({ limit: '50mb' }));
 app.use(require("./middleware/morgan"));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', require('./api'));
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
